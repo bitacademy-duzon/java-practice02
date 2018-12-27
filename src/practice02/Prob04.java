c2dc5585030f7c80e3eced668e0248544a21a618
CoRock <dycorphin@gmail.com>
2018-12-27 17:38:57 +0900

    Add Prob04.java

--- src/practice02/Prob04.java
+++ src/practice02/Prob04.java
@@ -11,10 +11,25 @@ public class Prob04 {
 	}
 	
 	public static char[] reverse(String str) {
-		/* 코드를 완성합니다 */
+		char[] result = str.toCharArray();
+		int begin = 0;
+		int end = result.length - 1;
+		char temp;
+
+		while (end > begin) {
+			temp = result[begin];
+			result[begin] = result[end];
+			result[end] = temp;
+			begin++;
+			end--;
+		}
+		return result;
 	}
 
 	public static void printCharArray(char[] array){
-		/* 코드를 완성합니다 */
+		for (Character c : array) {
+				System.out.print(c);
+		}
+		System.out.println();
 	}
 }
